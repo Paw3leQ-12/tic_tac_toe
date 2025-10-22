20956f8b25578adf7e91dd6fb71a91d1f767c164
Lukasz <pijanowskilukasz1@gmail.com>
2025-10-22 20:53:17 +0200

    add style for grid

--- classes/grids.py
+++ classes/grids.py
@@ -1,12 +1,15 @@
-from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton
-from const import CELL_SIZE, CELL_COLOR, CELL_TEXT_COLOR
+from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
 
 class MainGrid(QWidget):
     def __init__(self):
         super().__init__()
         layout = QGridLayout()
+        layout.setSpacing(0)
         self.setLayout(layout)
+        self.setMinimumSize(450, 450)
         for row in range(3):
             for column in range(3):
-                button = QPushButton()
+                button = QPushButton("")
+                button.setProperty("class", "grid-button")
+                button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
                 layout.addWidget(button, row, column)
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-from PySide6.QtWidgets import QApplication, QMainWindow, QGridLayout
+from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QVBoxLayout
+from PySide6.QtCore import Qt
 from classes.grids import MainGrid
 
 app = QApplication([])
+
+with open("style.qss", "r") as styleSheet:
+    app.setStyleSheet(styleSheet.read())
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Tic-tac-toe")
-        self.setMinimumHeight(300)
-        self.setMinimumWidth(300)
+        self.setMinimumHeight(800)
+        self.setMinimumWidth(800)
+
+        layout = QVBoxLayout()
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+        self.setCentralWidget(centralWidget)
 
         mainGrid = MainGrid()
-        self.setCentralWidget(mainGrid)
+        
+        layout.addWidget(mainGrid, alignment=Qt.AlignCenter)
 
 window = MainWindow()
 window.show()
