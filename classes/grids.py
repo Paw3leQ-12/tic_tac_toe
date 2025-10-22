0a67881ebf1d1c3be41e8eda69210b571126a5ab
Lukasz <pijanowskilukasz1@gmail.com>
2025-10-22 22:31:44 +0200

    make click on button work

--- classes/grids.py
+++ classes/grids.py
@@ -1,4 +1,12 @@
 from PySide6.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+from PySide6.QtCore import Qt
+
+def turn_off_button(button: QPushButton):
+    button.setProperty("class", "grid-button-ckecked")
+    button.style().unpolish(button)
+    button.style().polish(button)
+    button.update()
+    button.setEnabled(False)
 
 class MainGrid(QWidget):
     def __init__(self):
@@ -9,7 +17,9 @@ class MainGrid(QWidget):
         self.setMinimumSize(450, 450)
         for row in range(3):
             for column in range(3):
-                button = QPushButton("")
+                button = QPushButton()
                 button.setProperty("class", "grid-button")
                 button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+                button.setCursor(Qt.PointingHandCursor)
+                button.clicked.connect(lambda checked = False, btn = button: turn_off_button(btn))
                 layout.addWidget(button, row, column)
